bf57595b4517bd9cb193e821298ca7805b42b7d7
UmranPros <152087084+umran666@users.noreply.github.com>
2025-09-05 14:46:46 +0530

    Pyhton-GUI-ChatBot.py

--- Pyhton-GUI-ChatBot.py
+++ Pyhton-GUI-ChatBot.py
@@ -1,13 +1,14 @@
-import tkinter as tk
-import google.generativeai as genai
-genai.configure(api_key="AIzaSyBjBWTxCnTUkPc7KQm-0Dj1bebTCnpXU_c")
+#install all Dependencies and Extensions
+import tkinter as tk  # pip install tkinter
+import google.generativeai as genai # pip install google.generativeai
+genai.configure(api_key="Your_api_Key") # Just copy and paste your Api Key
 model = genai.GenerativeModel("gemini-2.0-flash")
+
 def send():
     user_text = entry.get()
     if not user_text: return
     chat.insert(tk.END, "You: " + user_text + "\n")
     entry.delete(0, tk.END)
-
     reply = model.generate_content(user_text).text
     chat.insert(tk.END, "Gemini: " + reply + "\n\n")
 
@@ -15,12 +16,10 @@ root = tk.Tk()
 root.title("Gemini Chatbot")
 chat = tk.Text(root, wrap="word")
 chat.pack(padx=10, pady=10, fill="both", expand=True)
-
 entry = tk.Entry(root)
 entry.pack(side="left", fill="x", expand=True, padx=5, pady=5)
 entry.bind("<Return>", lambda e: send())
-
 send_btn = tk.Button(root, text="Send", command=send)
 send_btn.pack(side="right", padx=5, pady=5)
-
 root.mainloop()
+#Syntax By Umran666
